a4598a07052b80d82843da44dcd31f879ee1c43d
Lucas <lucasp222@hotmail.com>
2025-04-02 10:26:44 -0300

    Alteração dos templates, coloquei logo policia penal em todas as paginas

--- controle_veiculos_project/settings.py
+++ controle_veiculos_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-53rg2vr=q41!hq*desegbw#%m_5dv^xze5z2s(5wu#mc#=)--&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
